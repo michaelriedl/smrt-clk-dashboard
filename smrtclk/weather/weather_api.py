09654bf09f73bfef64e5133c0dcc7bdffd125f83
Michael Riedl <michael@michaelriedl.com>
2025-01-18 14:06:10 -0500

    Updated doc string with requirements

--- smrtclk/weather/weather_api.py
+++ smrtclk/weather/weather_api.py
@@ -20,4 +20,21 @@ class WeatherAPI(ABC):
 
     @abstractmethod
     def get_current_weather(self) -> dict:
+        """Gets the current weather for the given location. The return value is a dictionary with the weather data.
+
+        It is expected that the dictionary will have the following keys:
+        - temperature: The current temperature in degrees Fahrenheit.
+        - temperature_min: The minimum temperature for the day in degrees Fahrenheit.
+        - temperature_max: The maximum temperature for the day in degrees Fahrenheit.
+        - precipitation: The current chance of precipitation percentage.
+        - precipitation_min: The minimum chance of precipitation percentage for the day.
+        - precipitation_max: The maximum chance of precipitation percentage for the day.
+        - sunrise: The time of sunrise in the format HH:MM.
+        - sunset: The time of sunset in the format HH:MM.
+
+        Returns
+        -------
+        dict
+            The current weather data.
+        """
         pass
